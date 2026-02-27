d3abe8ebe9d2d3021d337e395fcc0301a7895e2f
Abhimanyu Verma <developer.abhimanyu.03@gmail.com>
2026-02-27 18:47:53 +0530

    db update temporary

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import path from "path";
 
-const dbPath = path.resolve("resumeiq.db");
+const dbPath = path.resolve("resumeiq1.db");
 
 const db = await open({
   filename: dbPath,
